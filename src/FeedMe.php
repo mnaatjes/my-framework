cfb3251d9fdc72121c3a3f44df99f7dbd57d7cd9
Michael Naatjes <michael.naatjes87@gmail.com>
2025-08-17 10:11:44 -0400

    feat: create FeedMe.php

---
+++ src/FeedMe.php
@@ -0,0 +1,13 @@
+<?php
+
+    namespace mnaatjes\App;
+
+    class FeedMe {
+        public function __construct(string $filepath){
+            // Load file from filepath
+
+            // Render information using a namespace defined in the composer.json file
+
+        }
+    }
+?>
